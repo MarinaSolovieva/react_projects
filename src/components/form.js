3173db2f16d55646ab8d396110abd771d9a05486
Marina Solovieva <marishasolovieva@mail.ru>
2020-02-10 00:10:44 +0300

    init commit

---
+++ src/components/form.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+class Form extends React.Component {
+    render() {
+        return (
+            <form onSubmit={this.props.WeatherMethod}>
+                <input type="text" name="city" placeholder="Город"/>
+                <button className="btn btn-danger btn-lg">Получить погоду</button>
+            </form>
+        );
+    }
+}
+
+export default Form;
